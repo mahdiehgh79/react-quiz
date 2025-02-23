5f110b664775cfc4f73db4308179a1b82fccbc83
mahdieh ghasemi <117377128+mahdiehgh79@users.noreply.github.com>
2025-02-23 12:34:06 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,102 @@
-import DateCounter from "./DateCounter.jsx";
+import Header from "./Header";
+import Content from "./Content";
+import { useEffect } from "react";
+import { useReducer } from "react";
+import Loader from "./Loader.jsx";
+import Error from "./Error.jsx";
+import StartScreen from "./StaetScreen.jsx";
+import Question from "./Question.jsx";
+import NextButtton from "./NextButton.jsx";
+import Progress from "./Progress.jsx";
+
+const initialstate = {
+  questions: [],
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
+    case "Newanswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "Nextquestion":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      };
+    default:
+      throw new Error("acton unknow");
+  }
+}
 
 function App() {
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
+    reducer,
+    initialstate
+  );
+
+  const numQuestions = questions.length;
+  const maxPoints = questions.reduce((prev, cur) => prev + cur.points, 0);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((resp) => resp.json())
+      .then((data) => dispatch({ type: "dataRecived", payload: data }))
+      .catch((error) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Content>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPoints={maxPoints}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButtton dispatch={dispatch} answer={answer} />
+          </>
+        )}
+      </Content>
     </div>
   );
 }
