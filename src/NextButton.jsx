8ccca4e6dd2c263fcb2940fb1f8b9d8939a028d3
mahdieh ghasemi <117377128+mahdiehgh79@users.noreply.github.com>
2025-02-23 16:57:31 +0330

    Update NextButton.jsx

--- src/NextButton.jsx
+++ src/NextButton.jsx
@@ -1,12 +1,29 @@
-const NextButtton = ({ dispatch, answer }) => {
+// import { useQuiz } from "../contexts/QuizContext";
+
+function NextButton({ dispatch, answer, index, numQuestions }) {
+  //   const { dispatch, answer, index, numQuestions } = useQuiz();
+
   if (answer === null) return null;
-  return (
-    <button
-      className="btn btn-ui"
-      onClick={() => dispatch({ type: "Nextquestion" })}
-    >
-      Next
-    </button>
-  );
-};
-export default NextButtton;
+
+  if (index < numQuestions - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "Nextquestion" })}
+      >
+        Next
+      </button>
+    );
+
+  if (index === numQuestions - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "finish" })}
+      >
+        Finish
+      </button>
+    );
+}
+
+export default NextButton;
